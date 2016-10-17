558c291e064bf17257434074950a496ead42d07b
Yu Li <liyu001989@gmail.com>
2016-10-17 09:57:53 +0800

    user composer

--- Applications/YourApp/start_gateway.php
+++ Applications/YourApp/start_gateway.php
@@ -17,8 +17,6 @@ use \GatewayWorker\Gateway;
 use \GatewayWorker\BusinessWorker;
 use \Workerman\Autoloader;
 
-// 自动加载类
-require_once __DIR__ . '/../../Workerman/Autoloader.php';
 Autoloader::setRootPath(__DIR__);
 
 // gateway 进程，这里使用Text协议，可以用telnet测试
